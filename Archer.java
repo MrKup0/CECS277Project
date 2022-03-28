71ef2f85525f160662ff24e188c76737d8d26481
MrKup0 <jacobpmitchell@gmail.com>
2022-03-28 13:24:07 -0700

    Initial Magical and JavaDocs for Archer and Magical

--- Archer.java
+++ Archer.java
@@ -5,13 +5,23 @@ public interface Archer {
      static final String ARCHER_MENU = "1. Arrow\n2. Fire Arrow";
      static final int NUM_ARCHER_MENU_ITEMS = 2;
 
+     /**
+      * Basic arrow attack method
+      * @param e the entity being attacked
+      * @return a string description of the attack
+      */
      public String arrow(entity e) {
           int dmg = (int) (Math.random() * 5) + 1; // Recomended damage, might tweak so hero does more dmg
           e.takeDamage(dmg);
           return this.getName() + " shoots at " + e.getName()
-          + " with an arrow for " + dmg + " points of damage."; 
+          + " with an arrow for " + dmg + " points of damage.";
      }
 
+     /**
+      * Fire arrow attack method
+      * @param e the entity being shoot at with a fire arrow
+      * @return the string description of the fire arrow attack
+      */
      public String fireArrow(entity e) {
           int dmg = (int) (Math.random() * 5) + 1; // Recomended damage, might tweak so hero does more dmg
           e.takeDamage(dmg);
--- Magical.java
+++ Magical.java
@@ -5,12 +5,27 @@
 public interface Magical {
      static final String MAGIC_MENU = "1. Magic Missile\n2. Fireball";
      static final int NUM_MAGIC_MENU_ITEMS = 2;
-
+     /**
+      * Iconic magic missle attack method
+      * @param e the entity being attacked
+      * @return a string description of the attack
+      */
      public String magicMissile(entity e) {
-          return "Not implemented";
+          int dmg = (int) (Math.random() * 5) + 1; // Recomended damage, might tweak so hero does more dmg
+          e.takeDamage(dmg);
+          return this.getName() + "casts Magic Missle on " + e.getName()
+          + " dealing " + dmg + " points of force damage.";
      }
 
+     /**
+      * Iconic fireball attack method
+      * @param e the entity being attacked
+      * @return a string description of the attack
+      */
      public String fireball(entity e) {
-          return "Not implemented";
+          int dmg = (int) (Math.random() * 5) + 1; // Recomended damage, might tweak so hero does more dmg
+          e.takeDamage(dmg);
+          return this.getName() + " casts Fireball on " + e.getName()
+          + " dealing " + dmg + " points of fire damage.";
      }
 }
